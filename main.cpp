d574b69f4e3bf1c45d4ede8ee8f5180579fb5940
DeanElizardo <del@delizardo.com>
2023-09-03 10:52:46 -0400

    Added some clarifing comments

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 
 int main()
 {
+    // Create a video mode object and set the screen resolution.
     sf::VideoMode vm(1920, 1080);
 
-    sf::RenderWindow(vm, "Timber!!!", sf::Style::Fullscreen);
+    // Create a window object and give it the video mode, a title
+    // and a style parameter
+    sf::RenderWindow window(vm, "Timber!!!", sf::Style::Fullscreen);
 
     return 0;
 }
