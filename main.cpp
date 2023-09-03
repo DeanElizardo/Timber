1832154970554c9f0ae3260908289a2eaf4aa8ac
DeanElizardo <del@delizardo.com>
2023-09-03 11:06:38 -0400

    Added basic game loop. Crashes?

--- main.cpp
+++ main.cpp
@@ -2,12 +2,46 @@
 
 int main()
 {
-    // Create a video mode object and set the screen resolution.
-    sf::VideoMode vm(1920, 1080);
+	// Create a video mode object and set the screen resolution.
+	sf::VideoMode vm(1920, 1080);
 
-    // Create a window object and give it the video mode, a title
-    // and a style parameter
-    sf::RenderWindow window(vm, "Timber!!!", sf::Style::Fullscreen);
+	// Create a window object and give it the video mode, a title
+	// and a style parameter
+	sf::RenderWindow window(vm, "Timber!!!", sf::Style::Fullscreen);
 
-    return 0;
+	// Primary game loop
+	while (window.isOpen())
+	{
+		/*
+		****************************************************
+		* Handle the player's input
+		****************************************************
+		*/
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		/*
+		****************************************************
+		* Update the scene
+		****************************************************
+		*/
+
+		/*
+		****************************************************
+		* Draw the scene
+		****************************************************
+		*/
+
+		// Clear everything from the last frame
+		window.clear();
+
+		// Draw the newly updated scene here
+
+		// Show the updated scene
+		window.display();
+	}
+
+	return 0;
 }
